f6ee2afab837c1d5d67006338a604cbcb96ff15c
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-15 18:38:03 +0530

    images added to json file and fetching into json

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const menuOpenBtn = document.getElementById('hamberger-menu-open');
 const slideMenu = document.getElementById('menu-mobile');
 const menuCloseBtn = document.getElementById('hamberger-menu-close')
 
+fetch('./images.json')
+    .then((res) => res.json())
+    .then((data) => imgData(data))
+
+/* Image Function */
+function imgData(image) {
+    console.log(image);
+}
 
 /* NavBar Open & Close */
 menuOpenBtn.addEventListener('click', menuOpen);
 menuCloseBtn.addEventListener('click', menuClose);
-window.addEventListener('mouseup', menuClose);
 
 function menuOpen() {
     slideMenu.style.animation = 'slide-in .6s';
