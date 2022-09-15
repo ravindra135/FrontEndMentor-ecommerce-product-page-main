55b665baf365fd8c90d95d78084602a8c0540fb8
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-15 23:38:09 +0530

    basic js added, work with dynamic images;

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // Variables
 const menuOpenBtn = document.getElementById('hamberger-menu-open');
 const slideMenu = document.getElementById('menu-mobile');
-const menuCloseBtn = document.getElementById('hamberger-menu-close')
+const menuCloseBtn = document.getElementById('hamberger-menu-close');
+const quantity = document.getElementById('quantity');
+let cartItem = 0;
 
-fetch('./images.json')
-    .then((res) => res.json())
-    .then((data) => imgData(data))
+/* Inventory Increase and Decrease */
+document.getElementById('inv-decrease').addEventListener('click', () => {
+    if (cartItem != 0) {
+        cartItem = cartItem - 1;
+        quantity.innerHTML = cartItem;
+    } else {
+        return null;
+    }
+});
 
-/* Image Function */
-function imgData(image) {
-    console.log(image);
-}
+document.getElementById('inv-increase').addEventListener('click', () => {
+    cartItem = cartItem + 1;
+    quantity.innerHTML = cartItem;
+});
+/* Inventory Increase and Decrease End */
 
-/* NavBar Open & Close */
+/* Navbar Open & Close */
 menuOpenBtn.addEventListener('click', menuOpen);
 menuCloseBtn.addEventListener('click', menuClose);
 
@@ -31,4 +40,3 @@ function menuClose() {
         document.querySelector('nav').classList.toggle('menuOpen');
     }
 }
-/* NavBar Open & Close Ends Here */
---
+++ fetch.js
@@ -0,0 +1,24 @@
+const productImage = [
+    {
+        "src": "./images/image-product-1.jpg",
+        "thumb": "./images/image-product-1-thumbnail.jpg"
+    },
+    {
+        "src": "./images/image-product-2.jpg",
+        "thumb": "./images/image-product-2-thumbnail.jpg"
+    },
+    {
+        "src": "./images/image-product-3.jpg",
+        "thumb": "./images/image-product-3-thumbnail.jpg"
+    },
+    {
+        "src": "./images/image-product-4.jpg",
+        "thumb": "./images/image-product-4-thumbnail.jpg"
+    }
+];
+
+const sliderImg = document.getElementById('slider-img-active');
+// productImage.forEach(element => {
+//     sliderImg.src = element.src;
+// });
+
