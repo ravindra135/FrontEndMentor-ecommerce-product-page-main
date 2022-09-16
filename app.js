6498352e248d364f4b4f86e112b01ea302514a85
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-16 11:56:41 +0530

    Cart Made Full Functional

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const menuCloseBtn = document.getElementById('hamberger-menu-close');
 const quantity = document.getElementById('quantity');
 const checkout = document.getElementById('checkout-btn');
 const cartCount = document.getElementById('cart-count');
+const headCart = document.getElementById('header-cart');
 let cartItem = 0;
 
 /* Inventory Increase and Decrease */
@@ -12,8 +13,6 @@ document.getElementById('inv-decrease').addEventListener('click', () => {
     if (cartItem != 0) {
         cartItem = cartItem - 1;
         quantity.innerHTML = cartItem;
-    } else {
-        return null;
     }
 });
 
@@ -25,14 +24,32 @@ document.getElementById('inv-increase').addEventListener('click', () => {
 
 /* Cart Process */
 
-// Checkout Button Click;
+// Show Cart-Items Pop Up on Click
+headCart.addEventListener('click', () => {
+    document.getElementById('cart').style.animation = 'showCart .7s ease-in';
+    document.getElementById('cart').classList.toggle('cart-open');
+    // If Cart is Empty;
+    if(cartItem == 0) {
+        document.getElementById('cart-products').classList.add('cart-empty');
+        document.getElementById('cart-msg').classList.remove('hidden');
+    } else {
+        document.getElementById('cart-products').classList.remove('cart-empty');
+        document.getElementById('cart-msg').classList.add('hidden');
+    }
+});
+
+// On Checkout Button Click;
 checkout.addEventListener('click', () => {
     if(cartItem != 0) {
         cartCount.classList.remove('hidden');
         document.getElementById('cart-count-quantity').innerHTML = cartItem;
+        // Setting the Quantity in Cart Popup;
+        let finalPrice = 125.00 * cartItem;
+        document.getElementById('inCart-quantity').innerHTML = cartItem;
+        document.getElementById('final-price').innerHTML = '$' + finalPrice + '.00'
     } else {
         cartCount.classList.add('hidden');
-        console.log('cant be added')
+        console.log('cant be added');
     }
 });
 
